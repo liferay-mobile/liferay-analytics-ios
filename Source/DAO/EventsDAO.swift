4c8deec2854218e071c080b302f0141c13a44da0
Allan Melo <allan.melo@liferay.com>
2018-04-27 15:25:33 -0300

    LPS-79149 Create EventsDAO class to parse and save events in FileStorage in framework directory

---
+++ Source/DAO/EventsDAO.swift
@@ -0,0 +1,69 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+
+import Foundation
+
+internal class EventsDAO {
+
+    init(fileStorage: FileStorage) {
+		self.fileStorage = fileStorage
+	}
+
+	func addEvents(events: [Event]) {
+		let currentEvents = getEvents()
+		let newEvents = currentEvents + events
+	
+		replaceEvents(events: newEvents)
+	}
+	
+	func getEvents() -> [Event]  {
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .iso8601
+		
+		guard
+			let events = fileStorage.getString(key: STORAGE_KEY_EVENTS),
+			let data = events.data(using: .utf8) else {
+				return []
+		}
+		
+		do {
+			return try decoder.decode([Event].self, from: data)
+		}
+		catch {
+			replaceEvents(events: [])
+			
+			return []
+		}
+	}
+	
+	func replaceEvents(events: [Event]) {
+		do {
+			let encoder = JSONEncoder()
+			encoder.dateEncodingStrategy = .iso8601
+			let newEventsData = try encoder.encode(events)
+			
+			guard let json = String(data: newEventsData, encoding: .utf8) else {
+				throw AnalyticsError.couldNotParseEvents
+			}
+			
+			try fileStorage.setString(key: STORAGE_KEY_EVENTS, value: json)
+		}
+		catch let error {
+			print("Could not replace events: \(error.localizedDescription)")
+		}
+	}
+	
+	let fileStorage: FileStorage
+	let STORAGE_KEY_EVENTS = "lcs_client_batch"
+}
