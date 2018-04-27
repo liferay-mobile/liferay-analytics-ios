6b80ac467a8ca7e8d440d0da904dab16fb57216e
Allan Melo <allan.melo@liferay.com>
2018-04-27 17:14:49 -0300

    LPS-80263 Init library method passing user properties and optional custom library properties; LPS-80264 Create set Identity method

---
+++ Source/Api/Bind/Analytics.swift
@@ -0,0 +1,134 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+
+import Foundation
+import UIKit
+
+/**
+- Author: Allan Melo
+*/
+public class Analytics {
+	
+	private init(
+		analyticsKey: String, flushInterval: Int, fileStorage: FileStorage) {
+		
+		self.analyticsKey = analyticsKey
+		
+		self.userDAO = UserDAO(fileStorage: fileStorage)
+		self.flushProgress = FlushProcess(
+			fileStorage: fileStorage, flushInterval: flushInterval)
+	}
+	
+	/**
+		Need to call method to initialize the library
+	
+		- Throws: `AnalyticsError.analyticsAlreadyInitialized` if the Analytics
+			library is already initialized.
+	*/
+	public class func `init`(
+		analyticsKey: String, flushInterval: Int = Analytics.DEFAUL_TIME_INTERVAL) throws {
+		
+		if let _ = sharedInstance {
+			throw AnalyticsError.analyticsAlreadyInitialized
+		}
+		
+		let fileStorage = try FileStorage()
+		
+		sharedInstance = Analytics(
+			analyticsKey: analyticsKey, flushInterval: flushInterval, fileStorage: fileStorage)
+	}
+	
+	/**
+		Need to call to clear the identity, to send events with anonymous session again.
+		Recommended after the user logout of application.
+	
+		- Throws: `AnalyticsError.analyticsAlreadyInitialized` if the Analytics
+		library is already initialized.
+	*/
+	public class func clearSession() {
+		let instance = try! Analytics.getInstance()
+		
+		instance.userDAO.clearSession()
+	}
+	
+	/**
+		Need to call to send events with user informations.
+		Recommended after the user login in application.
+	
+		- Throws: `AnalyticsError.analyticsAlreadyInitialized` if the Analytics
+		library is already initialized.
+	*/
+	public class func setIdentity(email: String, name: String? = nil) {
+		let instance = try! Analytics.getInstance()
+		
+		let identityContext = instance.getDefaultIdentityContext()
+		
+		identityContext.identityFields["email"] = email
+		identityContext.identityFields["name"] = name
+
+		clearSession()
+		instance.userDAO.setUserContext(identity: identityContext)
+	}
+	
+	/**
+		Send custom events to Analytics
+	
+		- Throws: `AnalyticsError.analyticsAlreadyInitialized` if the Analytics
+		library is already initialized.
+	*/
+	public class func send(
+		eventId: String, applicationId: String, properties: [String: String]? = nil) {
+		
+		let instance = try! Analytics.getInstance()
+		
+		instance.createEvent(eventId: eventId, applicationId: applicationId, properties: properties)
+	}
+	
+	func getDefaultIdentityContext() -> IdentityContextMessage {
+		return IdentityContextMessage(analyticsKey: analyticsKey) {
+			$0.touchSupport = true
+			$0.platform = "iOS"
+			
+			if let language = Locale.preferredLanguages.first {
+				$0.language = language
+			}
+		}
+	}
+	
+	class func getInstance() throws -> Analytics {
+		guard let sharedInstance = Analytics.sharedInstance else {
+			throw AnalyticsError.analyticsNotInitialized
+		}
+		
+		return sharedInstance
+	}
+	
+	func createEvent(eventId: String, applicationId: String, properties: [String: String]? = nil) {
+		let event = Event(applicationId: applicationId, eventId: eventId) {
+			if let properties = properties {
+				$0.properties = properties
+			}
+		}
+		
+		flushProgress.addEvents(event: event)
+	}
+	
+	internal static let DEFAUL_TIME_INTERVAL = 10
+	
+	internal static var sharedInstance: Analytics?
+	
+	internal let analyticsKey: String
+	internal let flushProgress: FlushProcess
+	internal let userDAO: UserDAO
+}
---
+++ Tests/Bind/AnalyticsTest.swift
@@ -0,0 +1,93 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+
+@testable import com_liferay_analytics_client
+import XCTest
+
+/**
+* @author Allan Melo
+*/
+class AnalyticsTest: XCTestCase {
+	
+	override func setUp() {
+		let fileStorage = try! FileStorage()
+		
+		let eventsDAO = EventsDAO(fileStorage: fileStorage)
+		try! fileStorage.setString(
+			key: eventsDAO.STORAGE_KEY_EVENTS, value: "[]")
+	}
+	
+	override func tearDown() {
+		Analytics.sharedInstance = nil
+	}
+	
+	func testInitAnalytics() {
+		do {
+			try Analytics.init(analyticsKey: "AnalyticsKey")
+			let instance = try Analytics.getInstance()
+			
+			assert(instance.analyticsKey == "AnalyticsKey")
+		}
+		catch {
+			assertionFailure()
+		}
+	}
+	
+	func testInitAnalyticsTwice() {
+		do {
+			try Analytics.init(analyticsKey: "AnalyticsKey")
+			try Analytics.init(analyticsKey: "AnalyticsKey")
+		}
+		catch let error {
+			assert(error is AnalyticsError)
+			let analyticsError = error as! AnalyticsError
+			
+			assert(analyticsError == .analyticsAlreadyInitialized)
+		}
+	}
+	
+	func testSendAnalytics() {
+		do {
+			try Analytics.init(analyticsKey: "AnalyticsKey")
+			Analytics.send(eventId: "eventId1", applicationId: "app1")
+			
+			let instance = try Analytics.getInstance()
+			let events = instance.flushProgress.eventsDAO.getEvents()
+			
+			assert(events.count == 1)
+		}
+		catch {
+			assertionFailure()
+		}
+	}
+	
+	func testSetIdentity() {
+		do {
+			try Analytics.init(analyticsKey: "AnalyticsKey")
+			Analytics.setIdentity(email: "email1", name: "name1")
+			
+			guard let identity = try Analytics.getInstance().userDAO.getUserContext() else {
+				assertionFailure()
+				
+				return
+			}
+			
+			assert(identity.identityFields["name"] == "name1")
+			assert(identity.identityFields["email"] == "email1")
+		}
+		catch {
+			assertionFailure()
+		}
+	}
+}
