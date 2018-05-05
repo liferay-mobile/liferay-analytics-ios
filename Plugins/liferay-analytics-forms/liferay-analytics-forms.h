4d57b997f5c4ed3a0199f2dcb29f74b2ab1fe49a
Allan Melo <allan.melo@liferay.com>
2018-05-05 11:06:24 -0300

    LPS-79160 Init Forms Plugin project

---
+++ Plugins/liferay-analytics-forms/liferay-analytics-forms.h
@@ -0,0 +1,19 @@
+//
+//  com_liferay_analytics_client_forms.h
+//  com-liferay-analytics-client-forms
+//
+//  Created by Allan Melo on 28/03/18.
+//  Copyright © 2018 Allan Melo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for com_liferay_analytics_client_forms.
+FOUNDATION_EXPORT double com_liferay_analytics_client_formsVersionNumber;
+
+//! Project version string for com_liferay_analytics_client_forms.
+FOUNDATION_EXPORT const unsigned char com_liferay_analytics_client_formsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <com_liferay_analytics_client_forms/PublicHeader.h>
+
+
